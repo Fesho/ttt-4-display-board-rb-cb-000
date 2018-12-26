85fa9fe0a7f031030e0a5ce7579f24019400d97a
Mostafa Elfishawy <fisho_world@yahoo.com>
2018-12-26 20:24:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,19 +2,16 @@
 
 
 board = ["  ", " ", " ", " ", "  ", "  ", "  ", "  ", "  "]
-def display_board(board)
 
-board = ["  ", " ", " ", " ", "  ", "  ", "  ", "  ", "  "]
-def display_board(board)
 
-  puts "   | #{board[1]} | #{board[2]}"
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
-end
-display_board(board)
-
+ def display_board(board)
+ 
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+ end
 
 
 
