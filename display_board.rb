a66aee1750807b9dc57cd1ad1531bc6f02ba5813
Mostafa Elfishawy <fisho_world@yahoo.com>
2018-12-26 21:25:07 +0000

    Automatically backed up by Learn

---
+++ display_board.rb
@@ -0,0 +1,64 @@
+board = ["  ", "  ", "  ", " ", "  ", "  ", "  ", "  ", "  "]
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+end
+display_board(board)
+
+puts" "
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+puts" "
+puts" "
+
+ board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts" "
+puts" "
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+
+display_board(board)
+
+puts" "
+puts" "
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts" "
+puts" "
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board(board)
+
+puts" "
+puts" "
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(board)
+
+puts" "
+puts" "
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board(board)
+
+puts" "
+puts" "
+
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+display_board(board)
+
+puts" "
+puts" "
+
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+display_board(board)
