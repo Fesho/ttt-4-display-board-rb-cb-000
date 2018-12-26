414520689edc3c55f700ab4f4728b3e071456d04
Mostafa Elfishawy <fisho_world@yahoo.com>
2018-12-26 20:37:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,11 +15,11 @@ board = ["  ", " ", " ", " ", "  ", "  ", "  ", "  ", "  "]
 display_board(board)
 
 
+puts" "
 
 
 
-
-board = ["  ", " ", " ", " x", "  ", "  ", "  ", "  ", "  "]
+board = ["  ", "  ", "  ", " ", " x  ", "  ", "  ", "  ", "  "]
 def display_board(board)
 
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
@@ -29,8 +29,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
 display_board(board)
+puts" "
 
-
-board = ["o  ", " ", "o ", " ", "  ", " o ", "  ", "  ", "  "]
+board = ["o ","  ", "  ", "  o", "  ", "  ", " o ", "  ", "  "]
 display_board(board)
-
